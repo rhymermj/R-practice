e0d14aad78ee58f2aa4207a437d7d9a6495de3d6
rhymermj <rhymermj@gmail.com>
2020-04-20 18:56:49 +1200

    Section 5 - Data Frames

--- data frames.R
+++ data frames.R
@@ -82,3 +82,26 @@ head(stats)
 head(stats)
 filter <- stats$Internet.users < 2
 stats[filter,]    # Display when the filter is true
+stats[stats$Birth.rate > 40,]
+stats[stats$Birth.rate > 40 & stats$Internet.users < 2,]
+stats[stats$Income.Group == "High income",]
+levels(stats$Income.Group)
+
+stats[stats$Country.Name == "Malta",]
+
+
+# Introduction to qplot() -----------------------------------------------
+library("ggplot2")
+?qplot()
+qplot(data = stats, x=Internet.users)
+qplot(data = stats, x=Income.Group, y=Birth.rate)
+qplot(data = stats, x=Income.Group, y=Birth.rate, size=I(3), colour=I("blue"))
+qplot(data = stats, x=Income.Group, y=Birth.rate, geom = "boxplot")
+
+
+# Visualizing with qplot ------------------------------------------------
+qplot(data = stats, x=Internet.users, y=Birth.rate)
+qplot(data = stats, x=Internet.users, y=Birth.rate, size=I(3))
+qplot(data = stats, x=Internet.users, y=Birth.rate, size=I(3), colour=I("red"))
+qplot(data = stats, x=Internet.users, y=Birth.rate, colour=Income.Group, size=I(2))
+# Analysis: higher income group has higher internet usage and lower birth rate.
